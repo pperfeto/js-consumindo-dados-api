92b71bd6f0caa648996b6746de7994f26b63adc2
pperfeto <paulo.perfetto@gmail.com>
2022-09-28 23:14:49 -0300

    final commit

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-var consultaCEP = fetch('viacep.com.br/ws/01001000/json/');
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP inexistente');
+        }
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
 
-console.log(consultaCEP);
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        mensagemErro.innerHTML = '<br><p>CEP inválido. Tente novamente</p><br>';
+        console.log(erro);
+    }
+}
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
